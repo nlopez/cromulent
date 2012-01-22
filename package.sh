e4bb6b44e5460bb68ca29653f01fbbc1bf249874
Nick Lopez <nlopez@gmail.com>
2012-01-22 12:58:06 -0500

    use svn rev for filename instead of date

--- package.sh
+++ package.sh
@@ -6,14 +6,14 @@ title="Chromium"
 applicationName="${title}.app"
 size="100m"
 tmp="$(mktemp -d /tmp/$(basename $0).XXXXXXXX)"
-dmg="${tmp}/${title}.dmg"
-dmg2="/Users/nlopez/Desktop/Chromium.dmg"
+dmg_tmp="${tmp}/${title}.dmg_tmp"
+dmg="/Users/nlopez/Dropbox/Public/Chromium-$(svn info /Users/nlopez/chromium/src/ | egrep "^Revision" | awk '{print $2}').dmg_tmp"
 
 hdiutil create -srcfolder "${src}" -volname "${title}" -fs HFS+ \
 	-fsargs "-c c=64,a=16,e=16" -format UDRW -size "${size}" \
-	"${dmg}"
+	"${dmg_tmp}"
 
-attach_output=$(hdiutil attach -readwrite -noverify -noautoopen "${dmg}")
+attach_output=$(hdiutil attach -readwrite -noverify -noautoopen "${dmg_tmp}")
 device=$(echo "${attach_output}" | egrep "^/dev/" | sed 1q | awk '{print $1}')
 mount=$(echo "${attach_output}" | sed -n "2p" | awk '{print $3}')
 
@@ -22,5 +22,5 @@ cp ./ChromiumDS_Store "${mount}/.DS_Store"
 chmod -Rf go-w "${mount}" || true
 sync
 hdiutil detach ${device}
-hdiutil convert "${dmg}" -format UDZO -imagekey zlib-level=9 -o "${dmg2}"
+hdiutil convert "${dmg_tmp}" -format UDZO -imagekey zlib-level=9 -o "${dmg}"
 rm -rf ${tmp}
