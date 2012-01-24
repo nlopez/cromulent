27dd1ad7aca19892bed1c87458ffb0053b9e986d
Nick Lopez <nlopez@gmail.com>
2012-01-23 20:54:20 -0500

    packages.sh now creates a dmg identical in appearance and layout to the one Google ships

--- package.sh
+++ package.sh
@@ -9,16 +9,19 @@ tmp="$(mktemp -d /tmp/$(basename $0).XXXXXXXX)"
 dmg_tmp="${tmp}/${title}.dmg"
 dmg="${HOME}/Dropbox/Public/Chromium-$(svn info ${HOME}/chromium/src/ | egrep "^Revision" | awk '{print $2}').dmg"
 
+# The magic numbers in fsargs are recommended by the hdiutil man page to
+# "minimize gaps at the front of the filesystem,
+# allowing resize to squeeze more space from the filesystem"
 hdiutil create -srcfolder "${src}" -volname "${title}" -fs HFS+ \
 	-fsargs "-c c=64,a=16,e=16" -format UDRW -size "${size}" \
 	"${dmg_tmp}"
 
-attach_output=$(hdiutil attach -readwrite -noverify -noautoopen "${dmg_tmp}")
-device=$(echo "${attach_output}" | egrep "^/dev/" | sed 1q | awk '{print $1}')
+# Use a randomly generated mount point name to ensure we don't unexpectedly clobber anything
+attach_output=$(hdiutil attach -readwrite -noverify -noautoopen -mountrandom /Volumes "${dmg_tmp}")
+device=$(echo "${attach_output}" | egrep "^/dev/" | sed -n "1p" | awk '{print $1}')
 mount=$(echo "${attach_output}" | sed -n "2p" | awk '{print $3}')
 
-ln -s /Applications "${mount}/Applications"
-cp ./ChromiumDS_Store "${mount}/.DS_Store"
+rsync -av $(dirname "${0}")/VolumeOverlay/ ${mount}/
 chmod -Rf go-w "${mount}" || true
 sync
 hdiutil detach ${device}
